2e7763303a64463b87258f160575d2b9a211ec36
Nedla <aldenwang1997@gmail.com>
2016-06-01 22:52:15 -0400

    Revert "Revert "Switched to C++""
    This reverts commit 11528fc322a3e847ffb283d05b4c30df58335ebc.

---
+++ The Legend of Nedla/The-Legend-of-Nedla/MainGame.cpp
@@ -0,0 +1,28 @@
+#include "MainGame.h"
+
+MainGame::MainGame()
+{
+	_window = nullptr; // nullptr?
+	_screenWidth = 1024;
+	_screenHeight = 768;
+}
+
+
+MainGame::~MainGame()
+{
+}
+
+
+
+void MainGame::run()
+{
+	initSystems();
+}
+
+void MainGame::initSystems()
+{
+	//	Initialize SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	_window = SDL_CreateWindow("The Legend of Nedla", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_OPENGL); // If the window creation fails it returns false
+}
